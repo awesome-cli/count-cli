d7cefcfe4a4b1ccabe514ac0d98005078e75ee96
Jakub Biesiada <biesiadajakub@icloud.com>
2020-05-07 00:05:42 +0200

    Clean up

--- lib/index.ts
+++ lib/index.ts
@@ -8,7 +8,9 @@ import rra from 'recursive-readdir-async';
 import ora from 'ora';
 
 import { collect } from './helpers/collect';
-import { isHidden } from './helpers/is-hidden';
+import { isHidden } from './helpers/isHidden';
+
+import { Params } from './interfaces/Params';
 
 const spinner = ora();
 
@@ -34,81 +36,71 @@ program
     collect,
     []
   )
-  .action(
-    async ({
-      recursive = false,
+  .action(async ({ recursive = false, exclude, include }: Params) => {
+    spinner.start('Checking directories');
+
+    const files = await rra.list('.', {
+      ignoreFolders: false,
+      recursive,
       exclude,
       include,
-    }: {
-      recursive: boolean | string;
-      exclude: string[];
-      include: string[];
-    }) => {
-      spinner.start('Checking directories');
+    });
 
-      const files = await rra.list('.', {
-        ignoreFolders: false,
-        recursive,
-        exclude,
-        include,
-      });
+    spinner.stop();
 
-      spinner.stop();
+    const hiddenFiles: string[] = [];
+    const visibleFiles: string[] = [];
 
-      const hiddenFiles: string[] = [];
-      const visibleFiles: string[] = [];
+    const visibleDirs: string[] = [];
+    const hiddenDirs: string[] = [];
 
-      const visibleDirs: string[] = [];
-      const hiddenDirs: string[] = [];
-
-      files.map((file: any) => {
-        if (file.isDirectory) {
-          if (isHidden(file.fullname)) {
-            hiddenDirs.push(file);
-          } else {
-            visibleDirs.push(file);
-          }
+    files.map((file: any) => {
+      if (file.isDirectory) {
+        if (isHidden(file.fullname)) {
+          hiddenDirs.push(file);
         } else {
-          if (isHidden(file.fullname)) {
-            hiddenFiles.push(file);
-          } else {
-            visibleFiles.push(file);
-          }
+          visibleDirs.push(file);
         }
-      });
+      } else {
+        if (isHidden(file.fullname)) {
+          hiddenFiles.push(file);
+        } else {
+          visibleFiles.push(file);
+        }
+      }
+    });
 
-      const allFiles = visibleFiles.length + hiddenFiles.length;
-      const allDirs = visibleDirs.length + hiddenDirs.length;
+    const allFiles = visibleFiles.length + hiddenFiles.length;
+    const allDirs = visibleDirs.length + hiddenDirs.length;
 
-      console.log(
-        chalk.cyanBright(
-          `Visible Files: ${visibleFiles.length}\n` +
-            `Hidden Files: ${hiddenFiles.length}\n` +
-            `All Files: ${allFiles}`
-        )
-      );
+    console.log(
+      chalk.cyanBright(
+        `Visible Files: ${visibleFiles.length}\n` +
+          `Hidden Files: ${hiddenFiles.length}\n` +
+          `All Files: ${allFiles}`
+      )
+    );
 
-      console.log('');
+    console.log('');
 
-      console.log(
-        chalk.greenBright(
-          `Visible Directories: ${visibleDirs.length}\n` +
-            `Hidden Directories: ${hiddenDirs.length}\n` +
-            `All Directories: ${allDirs}`
-        )
-      );
+    console.log(
+      chalk.greenBright(
+        `Visible Directories: ${visibleDirs.length}\n` +
+          `Hidden Directories: ${hiddenDirs.length}\n` +
+          `All Directories: ${allDirs}`
+      )
+    );
 
-      console.log('');
+    console.log('');
 
-      console.log(
-        chalk.yellowBright(
-          `All Visible: ${visibleFiles.length + visibleDirs.length}\n` +
-            `All Hidden: ${hiddenFiles.length + hiddenDirs.length}\n` +
-            `All: ${allFiles + allDirs}`
-        )
-      );
-    }
-  );
+    console.log(
+      chalk.yellowBright(
+        `All Visible: ${visibleFiles.length + visibleDirs.length}\n` +
+          `All Hidden: ${hiddenFiles.length + hiddenDirs.length}\n` +
+          `All: ${allFiles + allDirs}`
+      )
+    );
+  });
 
 program.on('--help', () => {
   console.log(
---
+++ lib/interfaces/Params.ts
@@ -0,0 +1,5 @@
+export interface Params {
+  recursive: boolean | string;
+  exclude: string[];
+  include: string[];
+}
